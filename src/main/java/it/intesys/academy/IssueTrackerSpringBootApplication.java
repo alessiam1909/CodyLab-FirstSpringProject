50a1b309e67c37882cff9c61f67853a9cbd3ad9b
Alessia <114435874+alessiam1909@users.noreply.github.com>
2023-06-08 11:46:58 +0200

    Add files via upload

---
+++ src/main/java/it/intesys/academy/IssueTrackerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package it.intesys.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/intesys/academy/IssueTrackerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package it.intesys.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
