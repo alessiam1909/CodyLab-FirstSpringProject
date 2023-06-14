17607231f6b9a1b311fa74e313176c9558068ed5
zeno_begnini <zeno.begnini@gmail.com>
2023-06-14 17:05:12 +0200

    controller updates

--- src/main/java/it/intesys/academy/controller/IndexController.java
+++ src/main/java/it/intesys/academy/controller/IndexController.java
@@ -1,6 +1,8 @@
 package it.intesys.academy.controller;
 
+import it.intesys.academy.dto.IssueDTO;
 import it.intesys.academy.dto.ProjectDTO;
+import it.intesys.academy.service.IssueService;
 import it.intesys.academy.service.ProjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,16 +11,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/mvc")
 public class IndexController {
 
 
     private final ProjectService projectService;
+    private final IssueService issueService;
 
-    public IndexController(ProjectService projectService) {
+    public IndexController(ProjectService projectService, IssueService issueService) {
 
         this.projectService = projectService;
+        this.issueService = issueService;
     }
 
     @GetMapping("/list-projects")
@@ -33,6 +39,8 @@ public class IndexController {
     @GetMapping("/project/{projectId}/")
     public String project(Model model, @PathVariable Integer projectId, @RequestParam String userName){
         ProjectDTO projectDTO = projectService.readProject(userName, projectId);
+        List<IssueDTO> issues = issueService.readIssues(userName, projectId);
+        model.addAttribute("issues", issues);
         model.addAttribute("username", userName);
         model.addAttribute("id", projectDTO.getId());
         model.addAttribute("project", projectDTO);
--- src/main/java/it/intesys/academy/controller/IssuesController.java
+++ src/main/java/it/intesys/academy/controller/IssuesController.java
@@ -1,9 +1,8 @@
 package it.intesys.academy.controller;
 
-import it.intesys.academy.dto.IssueDTO;
-import it.intesys.academy.dto.ProjectDTO;
+import it.intesys.academy.dto.CommentDTO;
+import it.intesys.academy.service.CommentService;
 import it.intesys.academy.service.IssueService;
-import it.intesys.academy.service.ProjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,22 +10,30 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/mvc")
 public class IssuesController {
 
 
     private final IssueService issueService;
+    private final CommentService commentService;
 
-    public IssuesController(IssueService issueService) {
+
+    public IssuesController(IssueService issueService, CommentService commentService) {
 
         this.issueService = issueService;
+        this.commentService = commentService;
     }
 //http://localhost:8080/mvc/issue/2/?userName=Zeno
     @GetMapping("/issue/{issueId}/")
     public String issue(Model model, @PathVariable Integer issueId, @RequestParam String userName) {
+        List<CommentDTO> comments = commentService.readComments(userName, issueId);
+        model.addAttribute("id", issueId);
         model.addAttribute("username", userName);
         model.addAttribute("issue", issueService.readIssue(userName, issueId));
+        model.addAttribute("comments", comments);
 
 
         return "issue";
